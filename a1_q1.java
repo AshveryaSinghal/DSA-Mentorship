5027f991bf47fc430ad6e95dc4e1850987a8c644
AshveryaSinghal <142670892+AshveryaSinghal@users.noreply.github.com>
2023-11-01 00:32:23 +0530

    Question 1 Assignment 1

---
+++ a1_q1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Write your code here
+System.out.println("*****");
+System.out.println("   * ");
+System.out.println("  *  ");
+System.out.println(" *   ");
+System.out.println("*****");
+  }
+}
+  }
+}
